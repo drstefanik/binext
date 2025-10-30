9d6cacb3a07f67f24e8786391257e107530d44c8
drstefanik <scicala@sightgroup.org>
2025-10-30 14:34:32 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'media',
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
@@ -7,7 +8,17 @@ export default {
       boxShadow: { soft: "0 10px 30px rgba(0,0,0,.06)" },
       backgroundImage: {
         "hero-gradient": "linear-gradient(180deg,#f6fbff,transparent 60%)",
-        "network": "url('/src/assets/network.svg')"
+        "network": "url('/src/assets/network.svg')",
+      },
+      keyframes: {
+        "gradient-x": {
+          '0%':   { backgroundPosition: '0% 50%' },
+          '50%':  { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
+      },
+      animation: {
+        "gradient-x": "gradient-x 12s ease infinite",
       },
     },
   },
